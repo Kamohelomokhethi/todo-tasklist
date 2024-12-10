97fe9388b973f4e216444ce92d9224f93fbc27db
Kamohelo Mokhethi <81868796+Kamohelomokhethi@users.noreply.github.com>
2024-12-11 00:10:48 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import axios from 'axios';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  
+  // Fetch tasks from the API
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get("https://todo-server-4bvr.onrender.com/api/todos");
+      setTodos(response.data);
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+    }
+  };
 
   return (
    <div className='task-container'>
@@ -8,9 +26,9 @@ function App() {
         <h2>list of all tasks</h2>
         <div className='all-tasks'>
           <ul>
-            <li>task 1</li>
-            <li>task 2</li>
-            <li>task 3</li>
+          {todos.map((t) => (
+            <li key={t.id}>{t.title}</li>
+          ))}
           </ul>
         </div>
       </div>
