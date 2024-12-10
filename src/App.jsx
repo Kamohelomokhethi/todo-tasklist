adb46d17d3cc698d01dc7d0942e8b4401372796c
kamohelomokhethi <kamohelomokhethi25@gmail.com>
2024-12-10 21:02:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='task-container'>
+      <div className='card'>
+        <h2>list of all tasks</h2>
+        <div className='all-tasks'>
+          <ul>
+            <li>task 1</li>
+            <li>task 2</li>
+            <li>task 3</li>
+          </ul>
+        </div>
+      </div>
+   </div>
+  )
+}
+
+export default App
